fd16212df6f8131bc6475ad89ee60789d778e935
ipek <ipekcivicakan@gmail.com>
2018-12-10 16:35:48 -0800

    on-demand sign in and sign out

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -22,13 +22,36 @@ class GoogleAuth extends Component {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() })
     }
 
+    signInHandler = () => {
+        this.auth.signIn();
+
+    }
+
+    signOutHandler = () => {
+        this.auth.signOut();
+    }
+
     renderAuthButton = () => {
         if (this.state.isSignedIn === null) {
-            return <div>I don't know if we are signed in </div>
+            return null;
         } else if (this.state.isSignedIn) {
-            return <div>I am signed in</div>
+            return (
+                <button
+                    className="ui red google button"
+                    onClick={this.signOutHandler}>
+                    <i className="google icon" />
+                    Sign Out
+            </button>
+            );
         } else {
-            return <div>I am not signed in</div>
+            return (
+                <button
+                    className="ui red google button"
+                    onClick={this.signInHandler}>
+                    <i className="google icon" />
+                    Sign In with Google
+                </button>
+            )
         }
     }
 
