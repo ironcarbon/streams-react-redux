5032816ac8504a528ece402c5f15bd997f9f9165
ipek <ipekcivicakan@gmail.com>
2019-01-05 15:24:41 -0800

    cleaning up with CompenentWillMount

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -23,6 +23,10 @@ class StreamShow extends React.Component {
     this.buildPlayer();
   }
 
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
 
   buildPlayer() {
     if (this.player || !this.props.stream) {
